0bca8c65daeb5d92fc2318ae2add6f91021ff874
Wiratama Paramasatya <wiratamaparamasatya@gmail.com>
2019-04-21 12:35:37 +0700

    enchance vote service

--- src/main/java/com/personal/evote/core/repository/RunningVoteRepository.java
+++ src/main/java/com/personal/evote/core/repository/RunningVoteRepository.java
@@ -10,5 +10,5 @@ import java.util.UUID;
 
 @Repository
 public interface RunningVoteRepository extends JpaRepository<RunningVote, UUID> {
-    Optional<List<RunningVote>> findAllByCandidateCategoryId(UUID candidateCategoryId);
+    Optional<List<RunningVote>> findAllByCandidateCategoryIdAndVoterId(UUID candidateCategoryId, UUID voterId);
 }
